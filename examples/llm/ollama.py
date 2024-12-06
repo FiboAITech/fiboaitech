b2ac4993a81a5356cdf315363bbc3884b93e4588
Noah-joe-harris <191123524+Noah-joe-harris@users.noreply.github.com>
2024-12-06 22:10:00 +0100

    feat: add ollama node

---
+++ examples/llm/ollama.py
@@ -0,0 +1,25 @@
+from fiboaitech.nodes.llms import Ollama
+from fiboaitech.prompts import Prompt
+
+
+def run_ollama_node(prompt: Prompt):
+    ollama_node = Ollama(
+        model="ollama/qwq",
+    )
+    response = ollama_node.run(input_data={}, prompt=prompt)
+    return response
+
+
+prompt = Prompt(
+    messages=[
+        {
+            "role": "user",
+            "content": "Explain the concept of entropy.",
+        },
+    ]
+)
+
+
+if __name__ == "__main__":
+    response = run_ollama_node(prompt)
+    print(response)
